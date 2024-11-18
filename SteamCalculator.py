2131c964d08fad13c08bc09f4bcb978636672b6f
george10hany77 <georgehany064@gmail.com>
2024-11-18 11:54:39 +0100

    adding slight  modifications to the main class

--- SteamCalculator.py
+++ SteamCalculator.py
@@ -117,12 +117,13 @@ def main():
     temperature = Temperature(205)  # °C
     enthalpy = Enthalpy(1826.6)  # kJ/kg
     entropy = Entropy(6.6430)  # kJ/kg·K
-    s_volume = Specific_Volume(0.0021)  # m³/kg
+    s_volume = Specific_Volume(0.002)  # m³/kg
     internal_energy = InternalEnergy(873)  # kJ/kg
 
     calculator = SteamCalculator()
     calculator.temperature_with_internal_energy(temperature=temperature, internal_energy=internal_energy)
     calculator.display()
-
+    calculator.temperature_with_specific_volume(temperature=temperature, s_volume=s_volume)
+    calculator.display()
 if __name__ == "__main__":
     main()
