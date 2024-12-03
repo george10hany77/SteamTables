6f03773995eb6a33d286f74874293a3c9de03feb
George Hany <georgehany064@gmail.com>
2024-12-03 18:49:41 +0100

    updating steam_demo.py

--- test.py
+++ test.py
@@ -3,44 +3,48 @@ from STD_TYPES import *
 from steam_demo import SteamCalculator
 from library.iapws95 import IAPWS95
 
+
 def switchh(typ):
     match typ:
-        case "Temperature (°C)": typ = "T" ,
-        case "Pressure (MPa)" : typ = "P",
-        case "Enthalpy (kJ/kg)": typ = "h",
-        case "Entropy (kJ/kg·K)":typ="s",
-        case "Internal Energy (kJ/kg)": typ = "u",
-        case "Specific Volume (m³/kg)": typ="rho",
+        case "Temperature (°C)":
+            typ = "T",
+        case "Pressure (MPa)":
+            typ = "P",
+        case "Enthalpy (kJ/kg)":
+            typ = "h",
+        case "Entropy (kJ/kg·K)":
+            typ = "s",
+        case "Internal Energy (kJ/kg)":
+            typ = "u",
+        case "Specific Volume (m³/kg)":
+            typ = "rho",
     return typ
-
 
 
 def get_joke(event):
     #pydom["div#jokes"].html = ""+pydom[".dropdown-menu"][0].html
 
-    type1=pydom["span.one"][0].content
-    type2=pydom["span.two"][0].content
-    data1=float(pydom["#in1"].value[0])
-    data2=float(pydom["#in2"].value[0])
+    type1 = pydom["span.one"][0].content
+    type2 = pydom["span.two"][0].content
+    data1 = float(pydom["#in1"].value[0])
+    data2 = float(pydom["#in2"].value[0])
 
     type1 = switchh(type1)[0]
     type2 = switchh(type2)[0]
 
-    if(type1 == "T"):
-        data1 = data1+273.15
-    elif(type2=="T"):
-        data2 = data2+273.15
+    if (type1 == "T"):
+        data1 = data1 + 273.15
+    elif (type2 == "T"):
+        data2 = data2 + 273.15
 
+    if (type1 == "rho"):
+        data1 = (1 / data1)
+    elif (type2 == "rho"):
+        data2 = 1 / data2
 
-    if(type1 == "rho"):
-        data1 = (1/data1)
-    elif(type2=="rho"):
-        data2 = 1/data2
+    print(data1)
 
-
-    print(data1 )
-
-    if type1==type2:
+    if type1 == type2:
         pydom["div#jokes"].html = """
             <div class="alert alert-danger d-flex align-items-center" role="alert">
                 <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
@@ -50,7 +54,7 @@ def get_joke(event):
             </div>"""
         return
 
-    kwargss = {type1:data1,type2:data2}
+    kwargss = {type1: data1, type2: data2}
     water = IAPWS95(**kwargss)
     pydom["div#jokes"].html = f"""<div id="jokes">
                 <table class="table-responsive">
@@ -66,12 +70,12 @@ def get_joke(event):
                 </thead>
                 <tbody>
                     <tr>
-                    <td>{water.T-273.15}</td>
+                    <td>{water.T - 273.15}</td>
                     <td>{water.P}</td>
                     <td>{water.h}</td>
                     <td>{water.s}</td>
                     <td>{water.u}</td>
-                    <td>{1/water.rho}</td>
+                    <td>{1 / water.rho}</td>
                     </tr>
                     
                 </table>
