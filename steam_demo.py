bf3ab8f9f1ee05713c40249956b12aa901c4193e
George Hany <georgehany064@gmail.com>
2025-01-22 00:47:15 +0100

    making the default options temp and pressure

--- steam_demo.py
+++ steam_demo.py
@@ -2,22 +2,25 @@ from SteamCalculator import *
 
 
 def main():
-    pressure = Pressure(0.1)  # MPa
+    pressure = Pressure(111)  # MPa
     temperature = Temperature(122)  # °C
     enthalpy = Enthalpy(1826.6)  # kJ/kg
-    entropy = Entropy(7.6)  # kJ/kg·K
+    entropy = Entropy(1)  # kJ/kg·K
     s_volume = Specific_Volume(0.00061)  # m³/kg
     internal_energy = InternalEnergy(13111)  # kJ/kg
     x = X(0.1)
 
-    param = {"pressure": pressure, "internal_energy": internal_energy}
+    param = {"pressure": pressure, "entropy": entropy}
 
     calculator = SteamCalculator()
-    calculator.pressure_with_internal_energy(**param)
+    calculator.pressure_with_entropy(**param)
     calculator.display()
 
-    # phase, x = determine_phase(prop_1=pressure, prop_2=entropy)
-    # print(f"Phase: {phase.name}, My x: {x}")
+    phase, x = determine_phase(prop_1=pressure, prop_2=entropy)
+    print(f"Phase: {phase.name}, My x: {x}")
+
+    # calculator.pressure_with_x(pressure=pressure, x=x)
+    # calculator.display()
 
 
 if __name__ == "__main__":
